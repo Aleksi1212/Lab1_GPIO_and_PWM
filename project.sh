4b4afb4eda03709489c88b5f1bf64a74d5f39aca
Aleksi Noro <aleksinoro@gmail.com>
2025-03-22 19:44:45 +0200

    check_build_dir function and debug mode

--- project.sh
+++ project.sh
@@ -2,16 +2,26 @@
 set -eEuo pipefail
 
 if [ $# -eq 0 ]; then
-    echo "No arguments provided. Enter build, run or clean."
+    echo "Error: No arguments provided. Arguments: <build | run | debug | clean>"
     exit 1
 fi
 
 BUILD_DIR="build"
+PROJECT_NAME=$(<project_name.txt)
+
+check_build_dir() {
+    if [ ! -d "$BUILD_DIR" ]; then
+        if [ "${1:-}" == "create" ]; then
+            mkdir -p "$BUILD_DIR"
+        else
+            echo "Error: Build directory does not exist. Please run './project.sh build' first."
+            exit 1
+        fi
+    fi
+}
 
 if [ "$1" == "build" ]; then
-    if [ ! -d "$BUILD_DIR" ]; then
-        mkdir -p "$BUILD_DIR"
-    fi
+    check_build_dir "create"
 
     cd "$BUILD_DIR"
     cmake ..
@@ -21,16 +31,19 @@ if [ "$1" == "build" ]; then
 fi
 
 if [ "$1" == "run" ]; then
-    if [ ! -d "$BUILD_DIR" ]; then
-        echo "Project not built."
-        exit 1
-    fi
+    check_build_dir
 
-    PROJECT_NAME=$(<project_name.txt)
     cd "$BUILD_DIR"
     openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 8000; program $PROJECT_NAME.elf verify reset exit"
 
     exit 0
+fi
+
+if [ "$1" == "debug" ]; then
+    check_build_dir
+
+    gnome-terminal -- bash -c "gdb-multiarch $BUILD_DIR/$PROJECT_NAME.elf; exec bash"
+    openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 8000"
 fi
 
 if [ "$1" == "clean" ]; then
