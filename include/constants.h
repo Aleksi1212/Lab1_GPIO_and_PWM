23180eb224ada5f934abfa9f89b2678580624072
Aleksi Noro <aleksinoro@gmail.com>
2025-03-22 19:02:35 +0200

    constants

---
+++ include/constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define LED_COUNT 3
+
+#define LED_1 20
+#define LED_2 21
+#define LED_3 22
+
+#define SW_0 9
+#define SW_1 8
+#define SW_2 7
+
+#define CLOCK_DIVIDER 125.0f
+#define MAX_LED_LEVEL 999
+
+#endif
